6eee9e369e1c913b3c3df657acc94ea5192df8d1
Yingjie <52tiffany.yang@gmail.com>
2022-12-12 16:33:11 -0500

    update

--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -2,7 +2,9 @@ package io.zipcoder;
 import io.zipcoder.utils.Item;
 import io.zipcoder.utils.ItemParseException;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -10,43 +12,31 @@ import java.util.regex.Pattern;
 public class ItemParser {
     public List<Item> parseItemList(String valueToParse) {
 
-        String singleItemMy = valueToParse.toLowerCase();
-        Pattern pattern = Pattern.compile("(?=).*?(?>##)");
-        Matcher matcher = pattern.matcher(singleItemMy);
-
-        ArrayList<Item> list = new ArrayList<>();
-        while (matcher.find()) {
-            try {
-                list.add(parseSingleItem(matcher.group(0)));
-            }
-            catch(ItemParseException e){
-            }
+      String myString = valueToParse.toLowerCase();
+      List<Item> myList = new ArrayList<>();
+      Pattern pattern = Pattern.compile("(?=).*?(?>##)");
+      Matcher matcher = pattern.matcher(myString);
+      while(matcher.find()){
+          try {
+              myList.add(parseSingleItem(matcher.group(0)));
+          }catch(ItemParseException e){
+          }
         }
-        return list;
+      return myList;
     }
 
-    public Item parseSingleItem(String singleItem) throws ItemParseException {
-        String singleItemMy = singleItem.toLowerCase();
-        Pattern pattern = Pattern.compile("(?<=[:, @, ^, *, %]).*?(?=[;#])");
-        Matcher matcher = pattern.matcher(singleItemMy);
-        ArrayList<String> list = new ArrayList<>();
+    public Item parseSingleItem (String singleItem) throws ItemParseException {
+        String myStr = singleItem.toLowerCase();
+        String[] newArr = myStr.split("[:,@,*,%,^,#,;]");
+       // System.out.println(Arrays.asList(newArr).toString()+ newArr.length);
 
-        while (matcher.find()) {
-            list.add(matcher.group(0));
+        List<String> list = Arrays.asList(newArr);
 
-        }
-
-        if (list.size()==4) {
-
-            double d = Double.parseDouble(list.get(1));
-
-
-            Item item = new Item(list.get(0), d, list.get(2), list.get(3));
-            return item;
-        }
-        else {
+        if(list.size()==8){
+            Double price = Double.parseDouble(list.get(3));
+            return  new Item(list.get(1),price, list.get(5),list.get(7));
+        }else{
             throw new ItemParseException();
-        }
-
+     }
     }
 }
